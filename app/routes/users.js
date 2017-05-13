940f07de27d0f6cbda3db5b0938ad4435dd4eb22
Gilles BERTRAND <gilles@triptyk.eu>
2017-05-13 14:57:59 +0200

    correct integration of lists and task in databases

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
      model(){
-          return Ember.RSVP.hash({
-            users: this.store.findAll('user'),
-          });
+          return  this.store.findAll('user');
     }
 });
--- app/routes/users/user/list.js
+++ app/routes/users/user/list.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
         model(params){
-          this.store.query('task',{list: params.list_id}).then(function(tasks){
-            console.log(tasks.get('length'))
-            return tasks;
-          });
+        return this.store.findRecord('list', params.list_id);
     }
 });
