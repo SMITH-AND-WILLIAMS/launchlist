3b84b95263ad1de3d7144ae15c829ffeaa4d2393
Gilles BERTRAND <gilles@triptyk.eu>
2017-05-15 21:19:25 +0200

    ajout du système de login et de verification

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,11 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    email:'test@test.com',
-    password:'testpassword',
+    session: Ember.inject.service(),
+    email:null,
+    password:null,
     actions :{
         login(){
-            console.log(this.get('email'), this.get('password'));
-        }
-    }
+           
+            this.get('session').login(this.get('email'),this.get('password'))
+            .then(()=> {
+                this.get('flashMessages').success('You have successfully signed in')
+                this.transitionToRoute('users.user',this.get('session').get('currentUser').get('id'));
+            })
+            .catch((errors)=>{
+          if(Ember.isArray(errors)){
+            errors.forEach((error)=>{
+                let message = `Error on ${error.attribute}: ${error.message}`;
+                this.get('flashMessages').danger(message,{sticky:true});
+            })
+          } else {
+            this.get('flashMessages').danger(errors.message,{sticky:true});
+          }
+      })
+        },
+    },
 });
---
+++ app/services/session.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  cookies: Ember.inject.service(),
+  store: Ember.inject.service(),
+  firebase: Ember.inject.service('firebaseApp'),
+  currentUser: null,
+  login(email, password) {
+    let auth = this.get('firebase').auth();
+    return auth.signInWithEmailAndPassword(email, password).then((firebaseUser) => {
+      let email = firebaseUser.email;
+      return this.get('store').query('user', {
+        orderBy: 'email',
+        equalTo: email
+      }).then((users) => {
+        let user = users.objectAt(0);
+        this.set('currentUser', user);
+        this.get('cookies').write('currentUserId', user.get('id'));
+      })
+    })
+
+  },
+  logout() {
+    this.set('currentUser', null);
+    this.get('cookies').clear('currentUserId');
+  },
+  init() {
+    this._super(...arguments);
+    let currentUserId = this.get('cookies').read('currentUserId');
+    if (currentUserId) {
+      let currentUser = this.get('store').findRecord('user', currentUserId);
+      this.set('currentUser', currentUser);
+    }
+  }
+});
