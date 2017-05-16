f01c98e7d0a3664abce81ee5a7673ca651743d5e
Gilles BERTRAND <gilles@triptyk.eu>
2017-05-16 13:46:39 +0200

    adaptation de la route pour améliorer le cheminement vers la liste

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,21 @@
+import { buildValidations, validator } from 'ember-cp-validations';
+
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-    name:DS.attr('string'),
-    email:DS.attr('string'),
-    lists:DS.hasMany('list',{async : true}),
+const { attr, hasMany } = DS;
+
+const Validations = buildValidations({
+    email:[
+        validator('presence',true),
+        validator('format',{type:'email'})
+    ],
+    password:[
+        validator('presence',true),
+        validator('length',{min:8})
+    ]
+});
+export default DS.Model.extend(Validations,{
+    name:attr('string'),
+    email:attr('string'),
+    lists:hasMany('list',{async : true}),
 });
