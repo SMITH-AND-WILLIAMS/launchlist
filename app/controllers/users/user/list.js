9f6afa147e3a073d5c5d63fec8ea1b5e40920cf3
Gilles BERTRAND <gilles@triptyk.eu>
2017-05-19 16:44:48 +0200

    add task in database, missing a lot of control before input

--- app/controllers/users/user/list.js
+++ app/controllers/users/user/list.js
@@ -1,10 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    session:Ember.inject.service(),
     actions:{
         toggleTaskStatus(param){
             param.set('done',!param.get('done'));
             param.save();
+        },
+        addTask(param){
+            let newTask = {
+                    action:param,
+                    done:false,
+                    list:this.get('model').id,
+            }
+            let task = this.store.createRecord('task',newTask);
+            this.get('model').get('tasks').addObject(task);
+            task.save().then(()=>{
+                this.get('model').save();
+            });
+           
+            
         }
     }
 });
