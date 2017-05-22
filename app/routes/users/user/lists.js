6bebef6a13907670106645192c0fd07456cbdb62
Gilles BERTRAND <gilles@triptyk.eu>
2017-05-22 13:25:42 +0200

    add list to database

--- app/routes/users/user/lists.js
+++ app/routes/users/user/lists.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
-        return this.modelFor('users.user').get('lists')
-    }
+    // model() {
+    //     return this.modelFor('users.user').get('lists')
+    // }
 });
